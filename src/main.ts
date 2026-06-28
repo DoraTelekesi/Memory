d764c44bb4a141faa1b09b20eb28e0d04e84d235
Dora Telekesi <dorothea0929@gmail.com>
2026-06-28 18:08:33 +0200

    set up homepage, with button animation

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
 import "./styles/style.scss";
 
+const buttonIconLeft = document.querySelector(".button__icon-left") as HTMLImageElement;
+const buttonIconRight = document.querySelector(".button__icon-right") as HTMLImageElement;
+
+const button = document.querySelector(".button") as HTMLButtonElement;
+
+button.addEventListener("mouseenter", () => {
+  buttonIconLeft.classList.add("rotateLeft");
+  buttonIconRight.classList.add("scale");
+});
+
+button.addEventListener("mouseleave", () => {
+  buttonIconLeft.classList.remove("rotateLeft");
+  buttonIconLeft.classList.add("transitionBack");
+  buttonIconRight.classList.remove("scale");
+  buttonIconRight.classList.add("transitionBack");
+});
