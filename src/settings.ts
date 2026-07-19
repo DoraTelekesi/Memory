3915815893cd6789d5db7bf6f2da8556b8d0818b
Dora Telekesi <dorothea0929@gmail.com>
2026-07-19 11:27:25 +0200

    style: add game theme visual on gaming theme selection

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,5 @@
 import "./styles/settings.scss";
+import { codeVibesSelectionTemplate, gamingSelectionTemplate, daProjectsSelectionTemplate, foodsSelectionTemplate } from "./templates/themes";
 
 //Radio Button variables
 const codeVibesSelection = document.getElementById("code-vibes-selection") as HTMLDivElement;
@@ -17,14 +18,28 @@ const selectedTheme = document.getElementById("selected-theme") as HTMLParagraph
 const selectedPlayer = document.getElementById("selected-player") as HTMLParagraphElement;
 const selectedSize = document.getElementById("selected-size") as HTMLParagraphElement;
 
+const themeVisualContent = document.getElementById("theme-visual-content") as HTMLDivElement;
+
 let themeSelection: (HTMLElement | null)[] = [codeVibesSelection, gamingSelection, daProjectsSelection, foodsSelection];
+let themeSelectionTemplate: (string | null)[] = [
+  codeVibesSelectionTemplate,
+  gamingSelectionTemplate,
+  daProjectsSelectionTemplate,
+  foodsSelectionTemplate,
+];
 
 themeSelection.forEach((selection) => {
-  selection?.addEventListener("click", () => {
+  selection?.addEventListener("click", (e) => {
+    // Ignore the duplicated event originating from the radio input
+    if ((e.target as HTMLElement).tagName.toLowerCase() === "input") {
+      return;
+    }
+
     if (selection) {
       selection.classList.add("is-selected");
       console.log(selection);
       selectedTheme.textContent = `${selection.textContent}`;
+      themeVisualContent.innerHTML = themeSelectionTemplate[themeSelection.indexOf(selection)] || "";
     }
     themeSelection.forEach((otherSelection) => {
       if (otherSelection !== selection) {
@@ -37,7 +52,11 @@ themeSelection.forEach((selection) => {
 let colorSelection: (HTMLElement | null)[] = [blueSelection, orangeSelection];
 
 colorSelection.forEach((selection) => {
-  selection?.addEventListener("click", () => {
+  selection?.addEventListener("click", (e) => {
+    // Ignore the duplicated event originating from the radio input
+    if ((e.target as HTMLElement).tagName.toLowerCase() === "input") {
+      return;
+    }
     if (selection) {
       selection.classList.add("is-selected");
       selectedPlayer.textContent = `${selection.textContent}`;
@@ -53,7 +72,11 @@ colorSelection.forEach((selection) => {
 let sizeSelection: (HTMLElement | null)[] = [smallSelection, mediumSelection, largeSelection];
 
 sizeSelection.forEach((selection) => {
-  selection?.addEventListener("click", () => {
+  selection?.addEventListener("click", (e) => {
+    // Ignore the duplicated event originating from the radio input
+    if ((e.target as HTMLElement).tagName.toLowerCase() === "input") {
+      return;
+    }
     if (selection) {
       selection.classList.add("is-selected");
       selectedSize.textContent = `${selection.textContent}`;
---
+++ src/templates/themes.ts
@@ -0,0 +1,53 @@
+export const codeVibesSelectionTemplate = `<div class="theme-visual">
+                <div class="theme-visual_header">
+                  <div class="theme-visual_header_players">
+                    <img src="assets/icon/label_blue.svg" alt="blue label" class="theme-visual_header_players_img" />
+                    <p class="theme-visual_header_players_text-blue">Blue</p>
+                    <p class="theme-visual_header_players_number-blue">0</p>
+                    <img src="assets/icon/label_orange.svg" alt="orange label" class="theme-visual_header_players_img" />
+                    <p class="theme-visual_header_players_text-orange">Orange</p>
+                    <p class="theme-visual_header_players_number-orange">6</p>
+                  </div>
+                  <div class="theme-visual_header_current">
+                    <p class="theme-visual_header_current_text">Current Player:</p>
+                    <img src="assets/icon/label_blue.svg" alt="blue label" class="theme-visual_header_current_img" />
+                  </div>
+                  <div class="theme-visual_header_exit">
+                    <img src="assets/icon/move_item.svg" alt="exit" class="theme-visual_header_exit_img" />
+                    <p class="theme-visual_header_exit_text">Exit Game</p>
+                  </div>
+                </div>
+                <div class="theme-visual_cards">
+                  <img src="assets/img/dev_akademie_card.png" class="theme-visual_cards_dev" alt="dev akademie card" />
+                  <img src="assets/img/git_card.png" class="theme-visual_cards_git" alt="git card" />
+                </div>
+              </div>`;
+
+export const gamingSelectionTemplate = `<div class="theme-visual-game">
+                <div class="theme-visual_header-game">
+                  <div class="theme-visual_header-game_players">
+                    <img src="assets/icon/chess_pawn_blue.svg" alt="orange pawn" class="theme-visual_header-game_players_img" />
+
+                    <p class="theme-visual_header-game_players_number-orange">6</p>
+                    <img src="assets/icon/chess_pawn_orange.svg" alt="blue pawn" class="theme-visual_header-game_players_img" />
+
+                    <p class="theme-visual_header-game_players_number-blue">2</p>
+                  </div>
+                  <div class="theme-visual_header-game_current">
+                    <p class="theme-visual_header-game_current_text">Current Player:</p>
+                    <img src="assets/icon/chess_pawn_blue_bg_white.svg" alt="blue label" class="theme-visual_header-game_current_img" />
+                  </div>
+                  <div class="theme-visual_header-game_exit">
+                    <img src="assets/icon/move_item_pink.svg" alt="exit" class="theme-visual_header-game_exit_img" />
+                    <p class="theme-visual_header-game_exit_text">Exit Game</p>
+                  </div>
+                </div>
+                <div class="theme-visual-game_cards">
+                  <img src="assets/img/Card_Back.png" class="theme-visual-game_cards_game" alt="game back card" />
+                  <img src="assets/img/Card_Front.png" class="theme-visual-game_cards_game-front" alt="game front card" />
+                </div>
+              </div>`;
+
+export const daProjectsSelectionTemplate = `<div class="theme-visual-da">`;
+
+export const foodsSelectionTemplate = `<div class="theme-visual-foods">`;
