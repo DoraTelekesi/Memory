b4215a0e38261af1150250435c2d1343d510b377
Dora Telekesi <dorothea0929@gmail.com>
2026-08-26 20:11:58 +0200

    style: add da-projects theme visual on theme selection

--- src/templates/themes.ts
+++ src/templates/themes.ts
@@ -48,6 +48,29 @@ export const gamingSelectionTemplate = `<div class="theme-visual-game">
                 </div>
               </div>`;
 
-export const daProjectsSelectionTemplate = `<div class="theme-visual-da">`;
+export const daProjectsSelectionTemplate = `<div class="theme-visual-da">
+                <div class="theme-visual_header-da">
+                  <div class="theme-visual_header-da_players">
+                    <img src="assets/icon/chess_pawn_blue.svg" alt="orange pawn" class="theme-visual_header-da_players_img" />
+
+                    <p class="theme-visual_header-da_players_number-orange">6</p>
+                    <img src="assets/icon/chess_pawn_orange.svg" alt="blue pawn" class="theme-visual_header-da_players_img" />
+
+                    <p class="theme-visual_header-da_players_number-blue">2</p>
+                  </div>
+                  <div class="theme-visual_header-da_current">
+                    <p class="theme-visual_header-da_current_text">Current Player:</p>
+                    <img src="assets/icon/current_player_dark_blue.svg" alt="blue label" class="theme-visual_header-da_current_img" />
+                  </div>
+                  <div class="theme-visual_header-da_exit">
+                    <img src="assets/icon/exit_blue.svg" alt="exit" class="theme-visual_header-da_exit_img" />
+                    <p class="theme-visual_header-da_exit_text">Exit Game</p>
+                  </div>
+                </div>
+                <div class="theme-visual-da_cards">
+                  <img src="assets/img/da_computer_card_2.png" class="theme-visual-da_cards_da" alt="game back card" />
+                  <img src="assets/img/da_shark_card.png" class="theme-visual-da_cards_da-front" alt="game front card" />
+                </div>
+              </div>`;
 
 export const foodsSelectionTemplate = `<div class="theme-visual-foods">`;
